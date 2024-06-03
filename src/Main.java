419f7c4b636f51a6e913ec78cddb2db2e87dba9a
Eduardo Rodrigues <eduxdudu@icloud.com>
2024-06-03 14:39:36 -0300

    final

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 import java.time.LocalDate;
@@ -21,9 +23,41 @@ public class Main {
         mentoria.setDescricao("Descrição mentoria JAVA");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
+        /*System.out.println(curso1);
         System.out.println(curso2);
 
-        System.out.println(mentoria);
+        System.out.println(mentoria);*/
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devEduardo = new Dev();
+        devEduardo.setNome("Eduardo");
+        devEduardo.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Eduardo" + devEduardo.getConteudosInscritos());
+        devEduardo.progredir();
+        devEduardo.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Inscritos Eduardo" + devEduardo.getConteudosInscritos());
+        System.out.println("Conteúdos Concluidos Eduardo" + devEduardo.getConteudosConcluidos());
+        System.out.println("XP: " + devEduardo.calcularTotalXp());
+
+        System.out.println("-------------------");
+
+        Dev devAline = new Dev();
+        devAline.setNome("Aline");
+        devAline.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Aline" + devAline.getConteudosInscritos());
+        devAline.progredir();
+        devAline.progredir();
+        devAline.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Inscritos Aline" + devAline.getConteudosInscritos());
+        System.out.println("Conteúdos Concluidos Aline" + devAline.getConteudosConcluidos());
+        System.out.println("XP: " + devAline.calcularTotalXp());
     }
 }
